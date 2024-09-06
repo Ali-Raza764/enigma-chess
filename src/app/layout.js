7b50fae7e58d7d6a9c68c2d94249d113c844e243
Ali-Raza764 <alikillerno@gmail.com>
2024-09-06 14:32:08 +0500

    Fix: Vercel Middleware issue

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import NextTopLoader from "nextjs-toploader";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,8 +13,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <NextTopLoader color="blue" showSpinner={false} />
         {children}
-        </body>
+      </body>
     </html>
   );
 }
--- src/middleware.js
+++ src/middleware.js
@@ -25,6 +25,8 @@ import { getToken } from "next-auth/jwt";
 export async function middleware(req) {
   const token = await getToken({ req, secret: process.env.AUTH_SECRET });
 
+  console.log("Token at vercel", token);
+  
   if (!token) {
     // Redirect to sign-in page if the token is not found
     return NextResponse.redirect(new URL("/", req.url));
