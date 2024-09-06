af5c14ac52e3e62af9e2b29aef9004cd647d6c1d
Ali-Raza764 <alikillerno@gmail.com>
2024-09-06 14:33:49 +0500

    Fix: Vercel MiddleWare issue

--- src/middleware.js
+++ src/middleware.js
@@ -23,7 +23,7 @@ import { NextResponse } from "next/server";
 import { getToken } from "next-auth/jwt";
 
 export async function middleware(req) {
-  const token = await getToken({ req, secret: process.env.AUTH_SECRET });
+  const token = await getToken({ req, secret: process.env.AUTH_SECRET, secureCookie });
 
   console.log("Token at vercel", token);
   
